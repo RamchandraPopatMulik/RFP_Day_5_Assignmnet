189a61e17fef1dc3b07475c2076be8539403a1f0
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-18 00:14:54 -0400

    added file

--- BaseCoreFlipConeQ1New/Program.cs
+++ BaseCoreFlipConeQ1New/Program.cs
@@ -2,33 +2,21 @@
 {
     internal class Program
     {
-        public static bool isPrime(int i)
-        {
-            for (int j = 2; j < i; j++)
-            {
-                if (i % j == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
         public static void Main(String[] args)
         {
-            Program u = new Program();
-            Console.WriteLine("Enter the no. to find PrimeFactors:");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("The prime factors for the " + n + " is:");
+            Console.WriteLine("Please Enter divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 2; i <= n; i++)
-            {
-                if (isPrime(i))
-                {
-                    Console.WriteLine(i + " ");
-                }
-            }
-            Console.WriteLine();
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend {0} Divisor {1}", dividend, divisor);
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
         }
     }
 }
