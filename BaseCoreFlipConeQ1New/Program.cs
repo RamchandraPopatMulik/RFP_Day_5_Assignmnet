09f07deb917676c86383d2d1a530f2281d673aa0
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-17 23:55:31 -0400

    added file

--- BaseCoreFlipConeQ1New/Program.cs
+++ BaseCoreFlipConeQ1New/Program.cs
@@ -4,28 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coin Flip Count");
-            double Toss = Convert.ToDouble(Console.ReadLine());
-            int HCount = 0;
-            int TCount = 0;
-            for (int i = 0; i < Toss; i++)
+            Console.WriteLine("Please Enter Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0)
             {
-                Random random = new Random();
-                int dice = random.Next(0, 2);
-                if (dice < 0.5)
-                {
-                    HCount += 1;
-                }
-                else
-                {
-                    TCount += 1;
-                }
-                Console.WriteLine("Head Count : " + HCount);
-                Console.WriteLine("Tail Count : " + TCount);
-                double Hpercent = ((double)HCount / Toss) * 100;
-                Console.WriteLine("Head Percentage is :" + Hpercent);
-                double Tpercent = ((double)TCount / Toss) * 100;
-                Console.WriteLine("Head Percentage is :" + Tpercent);
+                Console.WriteLine(" This is a Leap Year ");
+            }
+            else
+            {
+                Console.WriteLine(" This is a Not Leap Year ");
             }
         }
     }
