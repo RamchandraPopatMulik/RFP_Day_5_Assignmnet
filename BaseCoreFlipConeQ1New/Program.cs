fe4490a8972c1543c4829cfaa177ed1ba981b064
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-18 00:23:47 -0400

    added file

--- BaseCoreFlipConeQ1New/Program.cs
+++ BaseCoreFlipConeQ1New/Program.cs
@@ -4,19 +4,17 @@
     {
         public static void Main(String[] args)
         {
-            int temp;
-            Console.WriteLine("Enter Your First Number :");
-            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Your Number");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Your Second Number :");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            temp = a;
-            a = b;
-            b = temp;
-
-            Console.WriteLine("Value of a After Swaping :" + a);
-            Console.WriteLine("Value of b After Swaping :" + b);
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("THIS IS EVEN NUMBER");
+            }
+            else
+            {
+                Console.WriteLine("THIS IS ODD NUMBER");
+            }
         }
     }
 }
