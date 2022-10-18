158c68dac05c505f612e0ce3075d86c2d24ca0f9
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-17 23:50:01 -0400

    added file

---
+++ BaseCoreFlipConeQ1New/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BaseCoreFlipConeQ1New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coin Flip Count");
+            double Toss = Convert.ToDouble(Console.ReadLine());
+            int HCount = 0;
+            int TCount = 0;
+            for (int i = 0; i < Toss; i++)
+            {
+                Random random = new Random();
+                int di = random.Next(0, 2);
+                if (di < 0.5)
+                {
+                    HCount += 1;
+                }
+                else
+                {
+                    TCount += 1;
+                }
+                Console.WriteLine("Head Count : " + HCount);
+                Console.WriteLine("Tail Count : " + TCount);
+                double Hpercent = ((double)HCount / Toss) * 100;
+                Console.WriteLine("Head Percentage is :" + Hpercent);
+                double Tpercent = ((double)TCount / Toss) * 100;
+                Console.WriteLine("Head Percentage is :" + Tpercent);
+            }
+        }
+    }
+}
